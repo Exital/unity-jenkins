9fc43bf7d4c79b22eacc10ca4c44f0df1b180d97
Tal Rozenzweig <31258059+Exital@users.noreply.github.com>
2024-07-01 22:52:40 +0300

    Create Constants.groovy

---
+++ src/Constants.groovy
@@ -0,0 +1,7 @@
+package src
+
+class Constants {
+  static final String PORTFOLIO_PATH = 'Portfolio'
+  static final String SCM_URL_DSL_REPO = 'https://github.com/Exital/unity-jenkins'
+  static final String EXITAL_GITHUB_CREDENTIALS = 'some credentials id'
+}
