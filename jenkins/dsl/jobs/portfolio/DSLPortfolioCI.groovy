73f139ef42e26a6191e2827ced9a7ec66d5b3075
Tal Rozenzweig <31258059+Exital@users.noreply.github.com>
2024-07-01 22:48:04 +0300

    Create DSLPortfolioCI.groovy

---
+++ jenkins/dsl/jobs/portfolio/DSLPortfolioCI.groovy
@@ -0,0 +1,26 @@
+import src.CommonSteps
+
+CommonSteps.createPathFolders(this, Constants.PORTFOLIO_PATH)
+def JOB_NAME = Constants.PORTFOLIO_PATH + "/Portfolio-CI"
+
+pipelineJob(JOB_NAME){
+  triggers {
+    genericTrigger {
+        genericVariables {
+            genericVariable {
+                key("portfolio_branch")
+                value("\$.ref")
+            }
+        }
+        regexpFilterText("\$portfolio_branch")
+        regexpFilterExpression("^(refs\\/heads\\/(master|develop))*?\$")
+        printContributedVariables(true)
+        printPostContent(true)
+        tokenCredentialId('Portfolio-CI-Webhook-Token')
+    }
+  }
+
+  stringParam('docker_registry', 'harbor.talrozen.com', 'Docker Registry')
+
+  CommonSteps.applyCpsScm(delegate, Constants.SCM_URL_DSL_REPO, Constants.EXITAL_GITHUB_CREDENTIALS, params.dsl_branch_name, "jenkins/pipelines/portfolio/Jenkinsfile.PortfolioCI")
+}
